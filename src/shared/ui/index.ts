a2a0e7e9e95991f8e7d7913efd9ccff6a34e96e5
kimsman <kimjaejun10@gmail.com>
2026-03-10 23:48:00 +0900

    [Refactor] shared/ui 내비게이션 내보내기 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,4 @@
 export { Button } from "./button";
 export { Input } from "./input";
+export { Navigation } from "./navigation/navigation";
+export type { NavigationProps, NavItem, User as NavUser } from "./navigation/navigation";
