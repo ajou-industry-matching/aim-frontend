0c0472a3efc9010de31609fd85e2b70665d89180
kimsman <kimjaejun10@gmail.com>
2026-03-12 12:58:14 +0900

    [Refactor] shared/ui 페이지네이션 내보내기 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,4 @@
 export { Button } from "./button";
 export { Input } from "./input";
+export { Pagination } from "./pagination/pagination";
+export type { PaginationProps } from "./pagination/pagination";
