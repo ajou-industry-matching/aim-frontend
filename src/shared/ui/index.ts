7c9f23c5c92488b1de2f7aec0b5743dc45745ca4
kimsman <kimjaejun10@gmail.com>
2026-03-28 19:59:47 +0900

    [Refactor] shared/ui 모달 컴포넌트 내보내기 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,15 @@
 export { Button } from "./button";
-export { Input } from "./input";
+export { Input, Textarea, Select } from "./inputBox/inputBox";
 export { DropdownMenu, SelectDropdown } from "./dropdown";
 export { Pagination } from "./pagination/pagination";
 export type { PaginationProps } from "./pagination/pagination";
 export { Navigation } from "./navigation/navigation";
 export type { NavigationProps, NavItem, NavUser } from "./navigation/navigation";
+
+// Form components
+export * from "./form";
+export * from "./checkbox";
+export * from "./file-uploader";
+
+// Modal components
+export * from "./modal";
