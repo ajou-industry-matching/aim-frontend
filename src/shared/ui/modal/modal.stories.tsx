da69fd62428de27879fb395b7f31e782054cd58f
kimsman <kimjaejun10@gmail.com>
2026-04-01 15:27:49 +0900

    [Chore] build test

--- src/shared/ui/modal/modal.stories.tsx
+++ src/shared/ui/modal/modal.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Modal, ModalHeader, ModalContent, ModalFooter } from "./modal";
 import { Button } from "@/shared/ui/button/button";
