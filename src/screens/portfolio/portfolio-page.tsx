df6f753bcbb3025bc43621948179edacc74ff208
kimsman <kimjaejun10@gmail.com>
2026-06-07 11:44:32 +0900

    [Refactor] portfolio-page useEffect eslint-disable 주석 제거

--- src/screens/portfolio/portfolio-page.tsx
+++ src/screens/portfolio/portfolio-page.tsx
@@ -86,8 +86,6 @@ export const PortfolioListPage = () => {
     return () => {
       isCancelled = true;
     };
-    // queryKey가 keyword/page/sort/selectedTypes를 직렬화해 인코딩하므로 deps에서 생략
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthReady, isAuthenticated, queryKey]);
 
   const handleSearchSubmit = (nextKeyword: string) => {
