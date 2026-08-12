1c79636a91d7705dc1f73b838d7f0066c036ee4c
kwqnyewest <yejoon_kwon@naver.com>
2026-08-12 15:30:53 +0900

    [Fix] #130 - 정책 페이지 링크를 공개된 노션 URL로 변경
    app.notion.com 링크는 워크스페이스 멤버만 접근 가능해 외부 방문자에게 404가 떠서, 웹에 게시된 notion.site 공개 링크로 교체

--- src/screens/terms/terms.tsx
+++ src/screens/terms/terms.tsx
@@ -4,7 +4,7 @@ export const TermsPage = (): React.ReactElement => {
   return (
     <PolicyPage
       title="이용약관"
-      externalUrl="https://app.notion.com/p/3b9edd4769b480eaa70de529b16835d6?source=copy_link"
+      externalUrl="https://hollow-cow-caa.notion.site/3b9edd4769b480eaa70de529b16835d6?source=copy_link"
       externalLinkLabel="이용약관"
     />
   );
