a4fcbf93b5e2b8c7240acec81717a451bd621b3c
권예준 <yejoon_kwon@naver.com>
2026-08-20 23:11:39 +0900

    [Feat] #130 - 푸터 링크 페이지 구현 (#148)

---
+++ src/screens/privacy/privacy.tsx
@@ -0,0 +1,11 @@
+import { PolicyPage } from "@/shared/ui";
+
+export const PrivacyPage = (): React.ReactElement => {
+  return (
+    <PolicyPage
+      title="개인정보처리방침"
+      externalUrl="https://hollow-cow-caa.notion.site/3b9edd4769b480759148d5e454a3c255?source=copy_link"
+      externalLinkLabel="개인정보처리방침"
+    />
+  );
+};
---
+++ src/shared/ui/policy-page/index.ts
@@ -0,0 +1,2 @@
+export { PolicyPage } from "./policy-page";
+export type { PolicyPageProps } from "./policy-page";
